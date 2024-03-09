cb2debeab735d427da1a560a1fcf063f44f3e2a3
Wing <w.gayongan@gmail.com>
2024-03-09 21:35:52 +0000

    reconfig metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-import type { Metadata, ResolvingMetadata } from "next";
+import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Roboto({ subsets: ["latin"], weight: ["300", "400", "500"] });
+
+export const metadata: Metadata = {
+  title: "Wilfreno Gayongan",
+  description: "Wilfreno Gayongan (Wing) Web Developer",
+};
 
 export default function RootLayout({
   children,
