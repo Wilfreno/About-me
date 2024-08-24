9b9e312784cea2b2ed046d99f7fe4bd290ee7737
Wing-09 <143404160+Wing-09@users.noreply.github.com>
2024-08-24 20:12:35 +0000

    feature: create header ui for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
 
 export default function Header() {
   const pages = [
@@ -9,8 +11,8 @@ export default function Header() {
     { name: "Contact", link: "contact" },
   ];
   return (
-    <header className="fixed inset-x-0 top-0 flex justify-end py-3 px-5">
-      <nav className="space-x-5">
+    <header className="fixed inset-x-0 top-0 flex sm:justify-end py-3 px-3 sm:px-5 bg-transparent sm:backdrop-blur-md">
+      <nav className="space-x-5 hidden sm:inline-flex">
         {pages.map((page) => (
           <Link key={page.name} href={"#" + page.link}>
             <Button
@@ -24,6 +26,29 @@ export default function Header() {
           </Link>
         ))}
       </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="secondary" size="icon" className="sm:hidden p-1">
+            <Menu className="h-full w-auto" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="space-y-5 grid py-5">
+            {pages.map((page) => (
+              <Link key={page.name} href={"#" + page.link}>
+                <Button
+                  variant="ghost"
+                  key={page.name}
+                  type="button"
+                  className="font-semibold w-full justify-start"
+                >
+                  {page.name}
+                </Button>
+              </Link>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 }
